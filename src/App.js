622b4c9248e5b5133fecefc5e8660ffdc0a47935
franetti <franco.martinetti@gmail.com>
2023-10-11 02:45:44 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,72 +1,71 @@
-import React, {useRef , useEffect, useLayoutEffect} from 'react';
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Home from './Home/Home';
-import ScrollToTop from './ScrollToTop/ScrollToTop';
-import gsap from 'gsap';
+import React, { useRef, useEffect, useLayoutEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Home/Home";
+import ScrollToTop from "./ScrollToTop/ScrollToTop";
+import gsap from "gsap";
 
 function App() {
-  
   const load = gsap.timeline({
     paused: "true",
   });
-  let loader = useRef(null)
-  let progress = useRef(null)
-  let percent = useRef(null)
-  let bar = useRef(null)
-  let barc = useRef(null)
+  let loader = useRef(null);
+  let progress = useRef(null);
+  let percent = useRef(null);
+  let bar = useRef(null);
+  let barc = useRef(null);
 
   useEffect(() => {
-    window.scrollTo(0,0)
-    if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
-      window.scrollTo(0,0)
-    } 
+    window.scrollTo(0, 0);
+    // if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+    //   window.scrollTo(0, 0);
+    // }
+    //   load.to([percent, bar], {
+    //     duration: 0.2,
+    //     opacity: 0,
+    //     zIndex: -1,
+    //   });
+    //   load.to(progress, {
+    //     duration: 0.8,
+    //     width: "0%",
+    //   });
+    //   load.to(loader, {
+    //     visibility: "hidden",
+    //     zIndex: -1,
+    //   });
+  }, []);
 
-    load.to([percent, bar], {
-      duration: 0.2,
-      opacity: 0,
-      zIndex: -1,
-    });
-    load.to(progress, {
-      duration: 0.8,
-      width: "0%",
-    });
-    load.to(loader, {
-      visibility: 'hidden',
-      zIndex: -1
-    });
-  });
-
-  var id;
-  var width1 = 1;
+  // var id;
+  // var width1 = 1;
 
   function loading() {
-    id = setInterval(frame, 20);
+    // id = setInterval(frame, 20);
+    // setTimeout(enableScroll, 5000);
   }
 
-  function frame() {
-    if (width1 >= 100) {
-      clearInterval(id);
-      load.play();
-    } else {
-      width1++;
-      document.getElementById("barc").style.width = width1 + "%";
-      document.getElementById("percent").innerHTML = width1 + "%";
-    }
-  }
-   
+  // function frame() {
+  //   if (width1 >= 100) {
+  //     clearInterval(id);
+  //     load.play();
+  //   } else {
+  //     width1++;
+  //     document.getElementById("barc").style.width = width1 + "%";
+  //     document.getElementById("percent").innerHTML = width1 + "%";
+  //   }
+  // }
+
   window.addEventListener("load", (e) => {
-    window.scrollTo(0,0)
-    loading();
-  })
+    window.scrollTo(0, 0);
+    //disableScroll();
+  });
 
   return (
-    <div>      
-    <Router>
-      <ScrollToTop />
+    <div>
+      <Router>
+        <ScrollToTop />
         <div className="noise"></div>
         <div className="App">
-          <div class="loader" ref={(el) => (loader = el)}>
+          {/* <div class="loader" ref={(el) => (loader = el)}>
               <div class="progress" ref={(el) => (progress = el)}>
                 <div id="percent" ref={(el) => (percent = el)}>
                   1%
@@ -75,9 +74,11 @@ function App() {
                   <div id="barc" ref={(el) => (barc = el)}></div>
                 </div>
               </div>
-          </div>    
+          </div>     */}
           <Switch>
-            <Route path="/" exact><Home /></Route>
+            <Route path="/" exact>
+              <Home />
+            </Route>
           </Switch>
           {/* <div className="cursor-follower" ref={ el => cursor = el }></div> */}
         </div>
